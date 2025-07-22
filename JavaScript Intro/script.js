0a71aecb25d0966e2171918935ac06edbcfbdeee
Rohit Kumar <144145989+Rohit57915@users.noreply.github.com>
2025-07-22 12:50:06 +0530

    Add files via upload

---
+++ JavaScript Intro/script.js
@@ -0,0 +1,38 @@
+debugger;
+//console.log(a);
+// console.log(c)
+// console.log(e)
+//console.log("Hey");
+//console.log("Hello")
+//var a=20;
+//const b=30;
+//let c=40;
+//console.log("I am external javaScript");
+
+// Ways to create a variable in js
+
+// !1. var
+
+//var a; // declaration
+ //  a=20; // initailization
+
+ //  var b=30; // declaration + initialization within the same line
+ //  var b; // redeclaration is also possiable
+  // var b=50;//redeclaration + initalization is also possible
+
+   // ! let
+  // let c; // declaration
+ //  c=20; // initailization
+  // c=true;// reinitalization
+  // let d=50; //declaration + intialization within the same line
+ 
+   // let c; // redeclaration not possible
+
+   // let c=70; //redeclaration + reinitialization is also not possible
+
+   //! const
+
+   //const e; // Only declaration without intialization is not possible 
+   //const e=40; //Declaration + intialization is the only thing which is possible with const Keyword
+
+//
