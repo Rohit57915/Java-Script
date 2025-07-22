434b18f2aadf3c87799fb12103b3ce51a0ac94c9
Rohit Kumar <144145989+Rohit57915@users.noreply.github.com>
2025-07-22 12:50:53 +0530

    Add files via upload

---
+++ looping st/Indexscript.js
@@ -0,0 +1,54 @@
+// armgstone number
+
+/*let num=prompt("Enter a Number")
+let length=num.length;
+num=parseInt(int)
+
+let res=0;
+while(num!===0)
+{
+      let ld=num%10;
+    res+=id**length;
+    num=parseInt(num/10);
+}
+if(temp===res)
+{
+    console.log("Armstrong Number")
+}
+else{
+   console.log("Not Armstrong Number")
+
+}*/
+
+/*function sum(p1,p2)
+{
+    console.log("sum is" ,p1+p2)
+    return p1+p2;
+}
+let res=sum(10,20)
+console.log(res)
+*/
+// 
+
+fun();
+console.log("Start")
+let a=10;
+var b=20;
+const c=60;
+
+/*if(true){
+let a=40;
+var b=50;
+const c=60;
+}*/
+
+function fun(){
+    let e=70;
+    var f=80;
+    const g=90;
+    console.log(e+f)
+}
+fun()
+console.log("Excecution completed")
+
+
