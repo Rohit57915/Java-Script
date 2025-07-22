4aa5d1d391a068843f49366a33610af90e49e2ce
Rohit Kumar <144145989+Rohit57915@users.noreply.github.com>
2025-07-22 12:51:22 +0530

    Add files via upload

---
+++ Operator/script.js
@@ -0,0 +1,19 @@
+// ! Operator
+
+// ?Arithmetic Operator
+/*console.log(2+3)
+console.log(20-3)
+console.log(20*5)
+console.log(20/3)
+console.log(20%3)
+console.log(3%20)
+
+console.log(4**6)
+console.log(5**5)*/
+
+// ?Assignment Operator
+
+
+let num=parseInt(prompt("Enter the value"))
+console.log(typeof num)
+ 
